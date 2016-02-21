df4c6eb0dcf85edbb760e6ebffc809c9567164a6
nralbrecht <nralbrecht@web.de>
2016-02-21 14:12:28 +0100

    Add apostrophes

--- api/login.php
+++ api/login.php
@@ -4,9 +4,9 @@
 	if (!@empty($_GET['username']) && !@empty($_GET['password'])) {
 		if ($api->is_user_valid($_GET['username'], $_GET['password'])) {
 			print(json_encode([
-				username => $_GET['username'],
-				balance => $api->get_user_balance($_GET['username']),
-				tokken => $api->get_tokken($_GET['username'])
+				'username' => $_GET['username'],
+				'balance' => $api->get_user_balance($_GET['username']),
+				'tokken' => $api->get_tokken($_GET['username'])
 			]));
 			http_response_code(202);
 		} else {
